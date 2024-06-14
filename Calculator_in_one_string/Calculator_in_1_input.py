e57fb4b03942ab2a7b7e82d231f0da0b40675ffe
░▒▓█  Jheff AT  █▓▒░ <18588201+jheffat@users.noreply.github.com>
2024-06-13 18:29:47 -0700

    Update Calculator_in_1_input.py

--- Calculator_in_one_string/Calculator_in_1_input.py
+++ Calculator_in_one_string/Calculator_in_1_input.py
@@ -1,68 +1,41 @@
-import string
+def calc(exps):
+    #get the string into list. ex: "22+45-23" --> [22,'+',45,'-',23]
+    express=[];gn=""
+    if exps[0] in "+-":exps="0"+exps
+    for l in exps:
+        if l in ".0123456789":gn=gn+l      
+        if l in "^*/+-": 
+           express=express+[float(gn)]+[l]
+           gn=""
+    express=express+[float(gn)]  
+   #Here where it start the calculation; 
+    for o in "^*/+-": #-->every cycle it will use one operator to do the operation using the dictionary {oper}.
+        while o in express: #-->while an operator exist in the list [express] it will continue                     
+            try:            #   executing the process til there is no operators but the result.
+                pos=express.index(o)
+                result=oper[o](express[pos-1],express[pos+1])
+                _=[express.pop(pos-1) for _ in range(3)]
+                express.insert(pos-1,result)              
+            except:
+                return "Invalid Expression" 
+    return express
 
+#By creating a Dictionary {oper} it will help to do the operation. ex: print(oper['+'](5,4)) --> 9
 oper={"^":lambda a,b:a**b,
       "*":lambda a,b:a*b,
       "/":lambda a,b:a/b,
       "+":lambda a,b:a+b,
       "-":lambda a,b:a-b}
-
-def calc(expx):
-    group_num=""; express=[];
-   
-    for i in expx:
-        if i not in string.digits+"+-*/^":return "Expression not support"  
-        if i in ".0123456789":
-            group_num+=i
-        elif i in "+-*/^":
-            if len(group_num)>0:
-                express+=[float(group_num)]
-                group_num=""
-            express+=[i]   
-    express+=[float(group_num)]
     
-    
-    if len(express)<=2: 
-        try:
-            return float(expx)
-        except:
-            return "Invalid expression"
-  
-    #***
- 
-    if str(express[0]) in "+-":express=[0]+express
-          
-
-    for o in "^*/+-":
-        
-
-        while o in express:
-            try:
-                print(express)
-                pos=express.index(o)
-                p=oper[o](express[pos-1],express[pos+1])
-                express.pop(pos-1)
-                express.pop(pos-1)
-                express.pop(pos-1)
-                express.insert(pos-1,p)
-               
-            except:
-                return "Invalid Expression"
-                
-      
-   
- 
-  
-    return express
-    
-
-
 
 result=0.0
 print("\nBy Jheff Alberty 12/15/2022\n")
 print("Calculator in one input string with /*-+^")
-while  result!=0.1:
-    result=calc(input("Enter Expression: ")) 
-    print("=",result)
-
+while  results!=[0.1]:
+    try:
+        results=calc(input("Enter Expression: ")) 
+        print("=",results)
+    except:
+        print("Invalid Expression")
 
 #By Jheff Alberty /icodexys
